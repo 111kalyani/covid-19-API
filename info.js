56c67a8db18ba1a334e225395ca1b2da39a38178
111kalyani <89124574+111kalyani@users.noreply.github.com>
2021-09-30 22:29:16 +0530

    Add files via upload

---
+++ info.js
@@ -0,0 +1,18 @@
+function foo(){
+    let cc= document.getElementById("cname").value;
+    console.log(cc);
+    
+    async function foo1(){
+        let data = await fetch(`https://api.covid19api.com/total/dayone/country/${cc}`);
+        // console.log(data);
+        let result = await data.json();
+        // console.log(result);
+        for(var i in result){
+            console.log(`${cc}:${result[i].Confirmed}:${result[i].Deaths}:${result[i].Recovered}:${result[i].Date}`);
+    
+        }
+    
+    }
+    foo1();
+    }
+    
